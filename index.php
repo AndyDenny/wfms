6a468105e308d8489415c6894691464cc285cc02
AndyDenny <tp0yah@gmail.com>
2024-09-10 21:21:14 +0300

    lesson 1

--- index.php
+++ index.php
@@ -1,1 +1,24 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Урок 1</title>
+</head>
+<body>
+    <p>Привет мир</p>
+    <?php
+    // однострочный комментарий
+    # тоже однострочный комментарий
+    /*
+    это многострочный комментарий
+    здесь не выполняются команды
+    echo "Hello World!";    
+    */
+    echo "Hello world!";
+    ?>
+    <?php echo '<p>Hello World2!</p>'?>
+    <?php echo '<p>Hello World3!</p>'?>
+    <?php print '<p>Hello World4!</p>'?>
+</body>
+</html>
