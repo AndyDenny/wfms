b6d82433cb56ce919b978da391888999fa20eb2a
Andrey Denisov <andrey.denisov@tularegion.ru>
2024-09-11 15:02:41 +0300

    lesson 8

--- index.php
+++ index.php
@@ -1,37 +1,67 @@
 <?php
 error_reporting(-1);
+?>
+    <!doctype html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Document</title>
+    </head>
+    <body>
+<?php
 
-
-//$var = false;
-//(bool) - ф-ция явного приведения типа
-//var_dump((bool) 0); // false
-//var_dump((bool) ''); // false
-
-//$light = 'red';
-//
-//if ($light == 'green'){
-//    echo "we may go";
+//$i = 1;
+//while($i <= 10){
+//    echo $i;
+//    $i++;
 //}
+//echo "<table border='1'>";
+//$i = 1;
+//while($i <= 10){
+//    echo "<tr>";
+//    $n = 1;
+//    while($n <= 3){
+//        echo "<td>Row - {$i} Col - {$n}</td>";
+//        $n++;
+//    }
+//    echo "</tr>";
+//    $i++;
+//
+//}
+//echo "</table>";
 
-//var_dump(5 > 3); // true
-//var_dump(5 < 3); // false
-//>=
-//<=
-//!=
-//if (5 > 3)
-//    echo 'OK'; // инструкция в кол-ве одной строки может записываться без фигурных скобок
-
-$light = 'yellow';
-if ($light == 'green'){
-    if (5 > 3){
-        echo "5 > 3";
-    }
-    echo "we may go";
-} elseif ($light = 'yellow'){
-    echo "we may ready";
-}else{
-    echo "we must stop";
-}
+//$year = 1900;
+//echo "<select>";
+//while ($year < 2024){
+//    echo "<option>$year</option>";
+//    $year++;
+//}
+//echo "</select>";
+//$i = 1;
+//do{
+//    echo $i++ . '<br>';
+//}while($i <= 10);
+// HomeWork
+//echo "<table border='1'>";
+//$i = 1;
+//while ($i < 10) {
+//    echo "<tr>";
+//    $j = 1;
+//    while ($j < 10) {
+//        $k = $i * $j;
+//        echo "<td>{$i} x {$j} = {$k}</td>";
+//        $j++;
+//    }
+//    echo "</tr>";
+//    $i++;
+//}
+//echo "</table>";
 
 
 ?>
+    </body>
+    </html>
+
