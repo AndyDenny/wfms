edee7e8bc963428478fd2a62ef21499f91f8ebe5
Andrey Denisov <andrey.denisov@tularegion.ru>
2024-09-25 15:48:46 +0300

    Start OOP

--- classes/controllers/ControllerPage.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace classes\controllers;
-class ControllerPage
-{
-     function __construct(){
-        echo 'ControllerPage <br>';
-     }
-}
--- connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-$name_server = 'localhost';
-$name_db = 'develop';
-$name_user = 'develop';
-$password = 'develop';
-// jdbc:mariadb://localhost:33061/develop
-$db = @mysqli_connect($name_server,$name_user, $password, $name_db) or die("Error connecting to DB");
-//mysqli_set_charset($db,"utf8") or die("Wrong charset"); // перекодровка, в данном случае не требуется
