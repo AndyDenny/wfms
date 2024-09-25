e9f804494aa3597800c7945bdab8b6ed1735bccd
Andrey Denisov <andrey.denisov@tularegion.ru>
2024-09-25 17:31:16 +0300

    OOP lesson 3

--- index.php
+++ index.php
@@ -6,30 +6,17 @@ function debug($data){
 }
 require_once 'classes/Car.php';
 
-$car = new Car(); // создаем экземпляр класса - объект.
-//new - оператор создания обьекта
-debug($car);
 $car1 = new Car();
-$car1->color = 'черный'; // переопределяем св-ва объекта
+$car1->color = 'черный';
 $car1->brand = 'volvo';
 $car1->speed = 200;
-$car1->year = 2000; // добавляем св-во на лету - так делать не рекомендуется
-$car1->coloг = 'синий'; // чтобы избежать ошибок, получения нужных данных
-// ->  - оператор обращения к св-вам или методам объекта
-debug($car1);
+
 $car2 = new Car();
 $car2->color = 'белый';
 $car2->brand = 'BMW';
 $car2->speed = 150;
-debug($car2);
 
-echo "<h3>О моем авто:</h3>
-Марка: {$car1->brand}<br>
-Цвет: {$car1->coloг}<br>
-Кол-во колес: {$car1->wheel}<br>
-Год выпуска: {$car1->year}<br>
-Скорость: {$car1->speed}<br>
+echo $car1->getCarInfo();
+echo $car2->getCarInfo();
 
-
-";
 
