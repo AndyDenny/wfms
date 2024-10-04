da2e93086c17095a72dfed1a946ba563ec857ce9
AndyDenny <tp0yah@gmail.com>
2024-10-04 11:12:40 +0300

    OOP lesson 11

--- classes/NotebookProduct.php
+++ classes/NotebookProduct.php
@@ -1,12 +1,11 @@
 <?php
-class NotebookProduct extends Product{
+class NotebookProduct extends Product implements IGadget{
 
     public $cpu;
 
     public function __construct($name, $price, $cpu = null){
         parent::__construct($name, $price);
-        $this->cpu = $cpu;   
-
+        $this->cpu = $cpu;
     }
 
     public function getCpu(){
@@ -17,5 +16,14 @@ class NotebookProduct extends Product{
         $out = parent::getProduct();
         $out.= "Процессор: {$this->getCpu()}<br>";
         return $out;
+    }
+    
+    public function addProduct($name, $price, $numPages = 0){
+        var_dump($name);
+        var_dump($price);
+        var_dump($numPages);
+    }
+    public function getCase(){
+        
     }
 }
--- index.php
+++ index.php
@@ -8,18 +8,56 @@ function debug($data){
 
 require_once 'classes/Product.php';
 require_once 'classes/I3D.php';
-// require_once 'classes/NotebookProduct.php';
+require_once 'classes/IGadget.php';
+require_once 'classes/NotebookProduct.php';
 require_once 'classes/BookProduct.php';
 
+function offerCase(iGadget $product){ // Предваряя передачу объекта -
+    // указывается его тип(в данном случае принадлежность к интерфейсу),
+    // для получения нужного типа объекта
+    echo "Предлагаем чехол для устройства {$product->getName()}<br/>";
+    // вызывается метод класса переданного объекта
+}
+
+
+/*
+все-равно не понятно,зачем передавать интерфейс, если можно передать тип объекта?
+если есть, разные типы объектов, но схожие реализации.
+например - ноутбук, телефон, компьютер. Объекты разные, их много - имплементим один интерфейс
+и передаем его рядом, в параметр. Получается, внутрь реализации, придут только те объекты,
+которые принадлежат одному типу - электронные ус-ва, при этом книги не придут. При этом мы получаем,
+расширенную проверку типов "из коробки".
+*/
+
+
 $book = new BookProduct('Три мушкетера', 120, 378);
-// $notebook = new NotebookProduct('Asus',4598, 'Intel');
+$notebook = new NotebookProduct('Asus',4598, 'Intel');
 
 debug($book);
 // debug($notebook);
- 
-echo $book->getProduct();
-// echo $notebook->getProduct();
-$book->test();
 
-// $book->addProduct('test',890,150);
+offerCase($notebook);
+
+// class A{};
+// class B extends A{};
+// class C{};
+
+// $a = new A();
+// $b = new B();
+// $c = new C();
+
+// // instanceof - оператор проверки типа обьекта.
+// // Принадлежит объект тому или иному классу/интерфейсу. Возвращает true\false
+
+// var_dump($a instanceof A); // true
+// var_dump($b instanceof B); // true
+// var_dump($c instanceof C); // true
+// // тут все объекты своих классов
+// var_dump($c instanceof A); // false
+// // потому что "$с" не объект класса "А"
+// var_dump($b instanceof A); // true
+// // потому что "$b" объект класса "B",
+// // родителем которого является класс "А"
+
+
 
