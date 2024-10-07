43f02c036c4d3e12caed5de117988b5bf7e1ba68
Andrey.Denisov <Andrey.Denisov@tularegion.ru>
2024-10-07 17:13:35 +0300

    Own framework lesson 5

--- public/index.php
+++ public/index.php
@@ -4,4 +4,6 @@ require_once dirname(__DIR__) . "/config/init.php";
 require_once LIBS . "/functions.php";
 
 new \ishop\App();
-debug(\ishop\App::$app->getPropertiers());
+//debug(\ishop\App::$app->getPropertiers());
+
+//throw new Exception('Pagge NOT founde', 44);
