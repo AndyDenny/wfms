f4b566b4bce4007ae275e74d762f031bc5e7d2bc
Andrey.Denisov <Andrey.Denisov@tularegion.ru>
2024-10-11 16:56:09 +0300

    Public side CMS lesson 1

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -10,20 +10,8 @@ class MainController extends AppController
 {
 
     public function indexAction(){
-        $posts = R::findAll('test');
 //        $this->setMeta(App::$app->getProperty('shop_name'), 'MainPageDescription', 'MainPageKeywords');
         $this->setMeta('MainPageTitle', 'MainPageDescription', 'MainPageKeywords');
-$name = "John";
-$age = 32;
-$names = ['Andy','Jane','Mike'];
-$cache = Cache::instance();
-//$cache->delete('test');
-        $data = $cache->get('test');
-        if (!$data){
-            $cache->set('test', $names);
-        }
-        debug($data);
-        $this->set(compact('name','age', 'names','posts'));
     }
 
 }
--- app/views/Main/index.php
+++
@@ -1,7 +0,0 @@
-<h1>views/main/index</h1>
-
-<?php
-foreach ($posts as $post){
-    echo '<h3>' . $post->title . '</h3>';
-}
-?>
