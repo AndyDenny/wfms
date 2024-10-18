7f1805baa5cc632cc026e2f6f85dff756f130721
Andrey.Denisov <Andrey.Denisov@tularegion.ru>
2024-10-18 15:52:24 +0300

    Public side CMS lesson 15

--- app/controllers/ProductController.php
+++ app/controllers/ProductController.php
@@ -4,6 +4,7 @@
 namespace app\controllers;
 
 
+use app\models\Product;
 use RedBeanPHP\R;
 
 class ProductController extends AppController
@@ -19,13 +20,19 @@ class ProductController extends AppController
 
         $related = R::getAll('SELECT * FROM related_product JOIN product ON product.id = related_product.related_id WHERE related_product.product_id = ?',[$product->id]);
 
-//        TODO - vatched products
+        $p_model = new Product();
+        $p_model->setRecentlyViewed($product->id);
+        $r_viewed = $p_model->getRecentlyViewed();
+        $recentlyViewed = null;
+        if($r_viewed){
+            $recentlyViewed = R::find('product','id IN ('.R::genSlots($r_viewed).') LIMIT 3', $r_viewed);
+        }
 
         $gallery = R::findAll('gallery', 'product_id = ? ', [$product->id]);
 
 //        TODO - modification product
 
         $this->setMeta($product->title,$product->description,$product->keywords);
-        $this->set(compact('product','related', 'gallery'));
+        $this->set(compact('product','related', 'gallery', 'recentlyViewed'));
     }
 }
