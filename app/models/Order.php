80096eb21dd014128bc9ec153e1d8dab674a83d2
Andrey.Denisov <Andrey.Denisov@tularegion.ru>
2024-10-29 15:58:41 +0300

    Public side CMS lesson 35

--- app/models/Order.php
+++ app/models/Order.php
@@ -2,6 +2,7 @@
 
 namespace app\models;
 
+use ishop\App;
 use RedBeanPHP\R;
 
 class Order extends AppModel {
@@ -23,10 +24,26 @@ class Order extends AppModel {
             $sql_part .= "($order_id, $product_id, {$product['qty']}, '{$product['title']}', {$product['price']})," ;
         }
         $sql_part = rtrim($sql_part,',');
-        R::exec("INSERT INTO order_product (order_id, product_id, qty, title, price) VALUES {$sql_part}");
+        $id = R::exec("INSERT INTO order_product (order_id, product_id, qty, title, price) VALUES {$sql_part}");
     }
 
     public static function mailOrder($order_id, $user_email){
-
+        $transport = (new \Swift_SmtpTransport( App::$app->getProperty('smtp_host'),App::$app->getProperty('smtp_port'),App::$app->getProperty('smtp_protocol') ))
+            ->setUsername(App::$app->getProperty('smtp_login'))
+            ->setPassword(App::$app->getProperty('smtp_password'));
+        $mailer = new \Swift_Attachment($transport);
+        ob_start();
+        require  APP . '/views/mail/mail_order.php';
+        $body = ob_get_clean();
+        $message = (new Swift_Message("Order № {$order_id}"))
+            ->setFrom([App::$app->getProperty('smtp_login') => App::$app->getProperty('shop_name')])
+            ->setTo($user_email,App::$app->getProperty('admin_email'))
+            ->setBody($body, 'text/html');
+        $result = $mailer->send($message);
+            unset($_SESSION['cart']);
+            unset($_SESSION['cart.qty']);
+            unset($_SESSION['cart.sum']);
+            unset($_SESSION['cart.currency']);
+            $_SESSION['success'] = 'Thanks for order';
     }
 }
--- config/params.php
+++ config/params.php
@@ -3,6 +3,9 @@ return [
     'admin_email' => 'admin@email.com',
     'shop_name' => 'My custom shop',
     'pagination' => 3,
+    'smtp_host' => '',
+    'smtp_port' => '',
+    'smtp_protocol' => '',
     'smtp_login' => '',
     'smtp_password' => ''
 ];
