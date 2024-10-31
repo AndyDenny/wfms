c240ca9534ce6145e93d120a65a3e245079670a8
Andrey.Denisov <Andrey.Denisov@tularegion.ru>
2024-10-31 17:43:50 +0300

    Admin side CMS lesson 3

--- app/controllers/admin/MainController.php
+++ app/controllers/admin/MainController.php
@@ -8,8 +8,12 @@ use RedBeanPHP\R;
 class MainController extends AppController {
 
     public function indexAction(){
-
+        $counNewOrders = R::count('order',"status = '0'");
+        $countUsers = R::count('user');
+        $countProducts = R::count('product');
+        $countCategories = R::count('category');
         $this->setMeta('AdminMainPageTitle', 'AdminMainPageDescription', 'AdminMainPageKeywords');
+        $this->set(compact('counNewOrders','countUsers','countProducts','countCategories'));
     }
 
 }
