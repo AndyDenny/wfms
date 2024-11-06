862ff21e1b61e9507019f3c499053b094bab12b8
Andrey.Denisov <Andrey.Denisov@tularegion.ru>
2024-11-06 14:43:03 +0300

    Admin side CMS lesson 8

--- public/adminlte/myscript.js
+++ public/adminlte/myscript.js
@@ -2,3 +2,12 @@ $(".delete").on("click",function () {
    var res = confirm("Продолжить?");
    if(!res) return false;
 });
+
+$('.sidebar-menu a').each(function(){
+   var mylocation = window.location.protocol + '//' + window.location.host + window.location.pathname
+   var link = this.href;
+   if(link == mylocation){
+      $(this).parent().addClass("active");
+      $(this).closest('.treeview').addClass("active");
+   }
+});
