39e1d04bb18c8efb6e817cbfa78684e2582d2d79
Andrey.Denisov <Andrey.Denisov@tularegion.ru>
2024-11-08 16:48:08 +0300

    Admin side CMS lesson 15

--- app/controllers/admin/UserController.php
+++ app/controllers/admin/UserController.php
@@ -34,6 +34,25 @@ class UserController extends AppController{
         $this->layout = 'login';
     }
     public function editAction(){
+        if(!empty($_POST)){
+            $id = $this->getRequestID(false);
+            $user = new  \app\models\admin\User();
+            $data = $_POST;
+            $user->load($data);
+            if(!$user->attributes['password']){
+                unset($user->attributes['password']);
+            }else{
+                $user->attributes['password'] = password_hash($user->attributes['password'],PASSWORD_DEFAULT);
+            }
+            if(!$user->validate($data) || !$user->checkUnique()){
+                $user->getErrors();
+                redirect();
+            }
+            if($user->update('user',$id)){
+                $_SESSION['success'] = 'Изменения сохранены';
+            }
+            redirect();
+        }
         $user_id = $this->getRequestID();
         $user = R::load('user',$user_id);
         $orders = R::getAll("SELECT `order`.`id`, `order`.`user_id`, `order`.`status`,`order`.`date`,`order`.`update_at`,`order`.`currency`,
